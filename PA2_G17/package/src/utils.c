d4b16ca350d30a4e4e06bfc525d288839ad88687
Nolan Kelly <kell2490@umn.edu>
2021-10-29 16:42:44 -0500

    Added a bunch of the stuff from handout

---
+++ PA2_G17/package/src/utils.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include<string.h>
+#include "utils.h"
+
+
+/*
+	Searches for pattern in file and writes the entire line containing the pattern to STDOUT
+	Format for print: [path to file] : Line in the file
+	@Params:
+		path - path to the file
+		pattern : pattern to be searched
+*/
+void searchPatternInFile(char* path, char* pattern){
+
+	
+	FILE * fp;
+
+	fp = fopen(path, "r");
+	if (fp == NULL){
+		fprintf(stderr,"Error opening file: %s \n",path);
+		return;
+	}
+	
+	//Read file line by line and check if pattern exists in the line
+	char buffer[1001];
+	while(fgets(buffer, 1000, fp) != NULL){
+		if(strstr(buffer,pattern) != NULL){
+			printf("%s : %s", path, buffer);
+		}
+	}
+
+		
+	//Close fp
+	fclose(fp);
+}
